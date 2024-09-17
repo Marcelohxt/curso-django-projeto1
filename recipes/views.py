b14550ee194cb78ff17c321fa52982f173ef3a0b
marcelo_hxt@hotmail.com <marcelo_hxt@hotmail.com>
2024-09-17 17:16:59 -0300

    Ultimos comits

--- recipes/views.py
+++ recipes/views.py
@@ -6,7 +6,7 @@ from django.http  import HttpResponse
 
 def home(request):
     return render(request, 'recipes/pages/home.html',context={
-        "name": ""
+        "name": "Receitas da Vovó Zuila"
         })
 
 
